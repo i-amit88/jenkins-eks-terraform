982c529c210f7701124e85c524fefdaf65d707e9
i-amit88 <102894134+i-amit88@users.noreply.github.com>
2024-04-03 16:16:19 +0530

    added security group config

--- data.tf
+++ data.tf
@@ -1,23 +1,66 @@
-data "aws_ami" "example" {
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.7.0"
 
-  most_recent = true
-  owners      = ["amazon"]
+  name = "jenkins-vpc"
+  cidr = var.vpc_cidr
+                             
+  azs            = data.aws_availability_zones.azs.names
+  public_subnets = var.vpc_public_subnet
 
- 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "jenkins-vpc"
+    Terraform   = "true"
+    Environment = "dev"
   }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+  public_subnet_tags = {
+    Name = "jenkins-subnet"
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  igw_tags = {
+    Name = "jenkins igw"
   }
+
 }
 
-data "aws_availability_zones" "azs" {}
+#SG
+module "jenkins-security-group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "jenkins-sg"
+  description = "jenkins-server"
+  vpc_id      = module.vpc.vpc_id
+
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "Jenkins-server"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  tags = {
+    Name = "jenkins-sg" 
+  }
+}
